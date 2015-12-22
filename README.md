245dc6baaa96a2d41035e62c40ec317257754931
Jessica Hutchison <j@splatlabs.com>
2015-12-21 17:51:51 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Problem 1
+Using your favorite language, please model a Coach, a Student, and the relationship between them. Given a list of students and a list of coaches, please create an algorithm to distribute the students evenly between the coaches.
+For example, given 3 coaches and 20 students, the distribution should be as follows:
+Coach 1: 7
+Coach 2: 7
+Coach 3: 6
+
+Problem 2
+Some coaches may already have students. Please change your distribution algorithm so that the distribution correctly distributes the students even if the coaches already have students on their list.
+For example, given 3 coaches:
+Coach 1: 7 students
+Coach 2: 7 students
+Coach 3: 6 students
+And given a new list of 10 students, the final distribution would be:
+Coach 1: 10 students
+Coach 2: 10 students
+Coach 3: 10 students
+
+Notes
+The solution can be database driven – or not
+Tests can help prove your solution works
+Some solutions are more performant
---
+++ coach.rb
@@ -0,0 +1,78 @@
+# School is our main container (database) class; comments show ORM mappings
+# School has_many Coaches
+class School
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @coaches = []
+    end
+
+    def add_coaches(list)
+        list.each {|c|
+            @coaches.push( Coach.new(c) )
+        }
+    end
+    
+    def assign_students(students)
+        students.each {|s|
+            @coaches.min_by {|c| c.students.length }.students.push( Student.new(s) )
+        }
+    end
+    
+    def list()
+        puts "Results for: #{@name}"
+        @coaches.each {|c|
+            puts "Coach: #{c.name} (#{c.students.length})"
+            puts "Students: " + c.students.map {|s| s.name }.join(", ")
+        }
+    end
+
+    
+end
+
+# Coach has_many Students, has_a School
+class Coach
+    attr_accessor :name
+    attr_accessor :students
+
+    def initialize(name)
+        @name = name
+        @students = []
+    end
+    
+    def list()
+        # show coach + call show on all student objects
+        return @name + ": " + @students
+    end
+
+end
+
+# Student has_a coach
+class Student
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def list()
+       puts @name 
+    end
+
+end
+
+school = School.new("A School")
+
+school.add_coaches([*"a".."c"].map {|c| "coach-" + c })
+school.assign_students([*"a".."z"].map {|s| "student-" + s })
+school.list()
+
+puts "------- (adding coaches and students) --------"
+
+# more coaches is beyond spec, but seemed like a logical use case and shows flexibility of classes :)
+school.add_coaches([*"d".."e"].map {|c| "coach-" + c })
+
+school.assign_students([*"A".."Z"].map {|s| "student-" + s })
+school.list()
+
